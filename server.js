a95e60e7aefa1d02036bda83da5310ae85ef1cc7
Indhirani <indhirani1910@gmail.com>
2026-01-24 11:46:41 +0530

    Fix default route to login page

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+// FIRST root route
+app.get("/", (req,res)=> res.sendFile(__dirname + "/public/login.html"));
 
-app.get("/", (req,res)=> res.sendFile(__dirname+"/public/login.html"));
 app.get("/register", (req,res)=> res.sendFile(__dirname+"/public/register.html"));
 app.get("/home", (req,res)=> res.sendFile(__dirname+"/public/index.html"));
 app.get("/farmers", (req,res)=> res.sendFile(__dirname+"/public/farmers.html"));
 app.get("/crops", (req,res)=> res.sendFile(__dirname+"/public/crops.html"));
 
-const PORT=process.env.PORT ||3000;
-app.listen(PORT, ()=> console.log("Server sunning on port"+PORT));
+// AFTER routes
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log("Server running on " + PORT));
